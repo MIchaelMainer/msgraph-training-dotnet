a49d570cf96fb5f849257d7a430f05a69b252cd4
Mustafa Zengin <muzengin@microsoft.com>
2023-02-16 11:10:55 -0800

    add skeleton for scenarios

---
+++ AuthZTests/AllowAllRequests.cs
@@ -0,0 +1,15 @@
+namespace AuthZTests;
+
+public class AllowAllRequests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ AuthZTests/DisallowGETsWithoutSelect.cs
@@ -0,0 +1,15 @@
+namespace AuthZTests;
+
+public class AllGETsAreWithSelect
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
